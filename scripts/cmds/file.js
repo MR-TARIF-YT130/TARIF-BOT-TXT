420954ed90397bbfac8ed80286e310dfff607f8c
Sayadabot123 <saydabot123@gmail.com>
2024-11-19 15:41:30 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["61563779834031",];
+		const permission = ["61563779834031","61559804313665",];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage(" You don't have permission to use this command. 🫦", event.threadID, event.messageID);
 		}
